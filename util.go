7de578759b53854a6592a6e96f19a9b7f58d61b3
Jen-Chieh <jcs090218@gmail.com>
2020-02-12 14:00:54 +0800

    Add util.

---
+++ util.go
@@ -0,0 +1,18 @@
+/**
+ * $File: util.go $
+ * $Date: 2020-02-12 13:59:11 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
+package util
+
+// Return the sum of A and B.
+// @param a : A value.
+// @param b : B value.
+// @return Data pointer.
+func add(a *int, b *int) *int {
+	return a + b;
+}
